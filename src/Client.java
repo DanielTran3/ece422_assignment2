d624f3ac7c2bc06e09d26fa09e3e2063b3ccc9b4
DanielTran3 <dtran3@ualberta.ca>
2017-04-09 09:46:37 -0600

    testing connection

--- src/Client.java
+++ src/Client.java
@@ -1,21 +1,30 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
-
 public class Client {
     public void main (String args[]) {
-        int port = Integer.parseInt(args[0]);
-        String hostname = args[1];
 
         if (args.length != 2) {
 			System.out.println("Please Enter Only Two Inputs: Portnumber Hostname");
 			System.exit(0);
 		}
+        int port = Integer.parseInt(args[0]);
+        String hostname = args[1];
 
         try {
+            System.out.println("Connecting to Computer: " + hostname + " on port: " + port);
             Socket clientSocket = new Socket(hostname, port);
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInput()));
+            System.out.println("Connection Successful!");
+            PrintWriter writeToServer = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader readFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+            int i = 0;
+            while (i < 10) {
+            	writeToServer.println("hi %i" + i);
+            }
+            System.out.println(readFromServer.readLine());
         }
 
         catch (IOException e) {
--- src/ClientThread.java
+++ src/ClientThread.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ClientThread extends Thread {
@@ -17,7 +21,21 @@ public class ClientThread extends Thread {
 	}
 
 	public void run() {
-		
+		int i = 0;
+        while (i < 10) {
+            System.out.println("Server Connected to Client!");
+            PrintWriter writeToClient;
+			try {
+				writeToClient = new PrintWriter(this.clientSocket.getOutputStream(), true);
+	            BufferedReader readFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+	        	while (i < 10) {
+	        		System.out.println("Client: " + readFromClient.readLine());
+	        	}
+	            writeToClient.println("From Server");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+        }
 	}
 
 }
